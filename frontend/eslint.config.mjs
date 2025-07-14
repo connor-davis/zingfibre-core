b480fa7c1c82d163ba9797e3bedfa43cffb8547c
Connor Davis <connor.davis@thusa.co.za>
2025-07-14 10:51:50 +0200

    feat: Initial Commit

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,33 @@
+import pluginReact from 'eslint-plugin-react';
+
+import pluginJs from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
+  eslintPluginPrettierRecommended,
+];
