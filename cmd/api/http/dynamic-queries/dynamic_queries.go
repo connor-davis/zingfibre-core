2237cad83eb54b8af18da6f878b64e1aed0a6606
Connor Davis <connor.davis@thusa.co.za>
2025-09-04 09:08:07 +0200

    feat: remove dynamic queries SQL files and implement dynamic queries router in Go

---
+++ cmd/api/http/dynamic-queries/dynamic_queries.go
@@ -0,0 +1,38 @@
+package dynamicQueries
+
+import (
+	"github.com/connor-davis/zingfibre-core/cmd/api/http/middleware"
+	"github.com/connor-davis/zingfibre-core/internal/models/system"
+	"github.com/connor-davis/zingfibre-core/internal/mysql/radius"
+	"github.com/connor-davis/zingfibre-core/internal/mysql/zing"
+	"github.com/connor-davis/zingfibre-core/internal/postgres"
+	"github.com/gofiber/fiber/v2/middleware/session"
+)
+
+type DynamicQueriesRouter struct {
+	Postgres   *postgres.Queries
+	Zing       *zing.Queries
+	Radius     *radius.Queries
+	Middleware *middleware.Middleware
+	Sessions   *session.Store
+}
+
+func NewDynamicQueriesRouter(
+	postgres *postgres.Queries,
+	zing *zing.Queries,
+	radius *radius.Queries,
+	middleware *middleware.Middleware,
+	sessions *session.Store,
+) *DynamicQueriesRouter {
+	return &DynamicQueriesRouter{
+		Postgres:   postgres,
+		Zing:       zing,
+		Radius:     radius,
+		Middleware: middleware,
+		Sessions:   sessions,
+	}
+}
+
+func (r *DynamicQueriesRouter) RegisterRoutes() []system.Route {
+	return []system.Route{}
+}
--- internal/postgres/queries/dynamic_queries_child_queries.sql
+++
@@ -1,9 +0,0 @@
--- name: CreateDynamicQueryChildQuery :one
-INSERT INTO
-    dynamic_query_child_queries (
-        dynamic_query_id,
-        child_query_id
-    )
-VALUES
-    ($1, $2)
-RETURNING *;
